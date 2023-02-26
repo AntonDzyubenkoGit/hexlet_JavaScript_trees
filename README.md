0280acb68ed140076ee736c9c0fd2c7af57a49f6
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-26 16:01:51 +0300

    Функция generate(), которая создает и возвращает файловую систему

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hexlet_JavaScript_trees
 Мои решения задач для курса "JS: Деревья"
+
+
+### Вывод в консоль дерева с развернутой структурой
+- *console.log(JSON.stringify(tree, null, ' '));*
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-// Функция removeFirstLevel(), которая принимает на вход дерево, и возвращает новое, элементами которого являются дети вложенных узлов 
+import { mkdir, mkfile } from "@hexlet/immutable-fs-trees";
 
-const removeFirstLevel = (tree) => {
-  const newTree = tree.filter((el) => Array.isArray(el));
-  return newTree.flat();
+// Функция generate(), которая создает и возвращает файловую систему
+
+const generate = () => {
+  const tree = mkdir(
+    "nodejs-package",
+    [
+      mkfile("Makefile"),
+      mkfile("README.md"),
+      mkdir("dist"),
+      mkdir("__tests__", [mkfile("half.test.js", { type: "text/javascript" })]),
+      mkfile("babel.config.js", { type: "text/javascript" }),
+      mkdir("node_modules", [mkdir("@babel", [mkdir("cli", [mkfile("LICENSE")])])], { owner: "root", hidden: false }),
+    ],
+    { hidden: true }
+  );
+
+  return tree;
 };
 
-const tree1 = [[5], 1, [3, 4]];
-const tree2 = [1, 2, [3, 5], [[4, 3], 2]];
-
-console.log(removeFirstLevel(tree2));
+// console.log(JSON.stringify(generate(), null, "  "));
+console.log(generate());
