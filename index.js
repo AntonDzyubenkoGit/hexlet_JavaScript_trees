3d380aab20b0e0f72f6d1ebc2bf420dd61c50c52
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-26 17:45:10 +0300

    Функция compressImages(), которая принимает на вход директорию, находит внутри нее картинки и "сжимает" их в два раза

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-import { mkdir, mkfile } from "@hexlet/immutable-fs-trees";
+import { mkfile, mkdir, getChildren, getMeta, getName, isFile, isDirectory } from "@hexlet/immutable-fs-trees";
+import _ from "lodash";
 
-// Функция generate(), которая создает и возвращает файловую систему
+// Функция compressImages(), которая принимает на вход директорию, находит внутри нее картинки и "сжимает" их в два раза
 
-const generate = () => {
-  const tree = mkdir(
-    "nodejs-package",
-    [
-      mkfile("Makefile"),
-      mkfile("README.md"),
-      mkdir("dist"),
-      mkdir("__tests__", [mkfile("half.test.js", { type: "text/javascript" })]),
-      mkfile("babel.config.js", { type: "text/javascript" }),
-      mkdir("node_modules", [mkdir("@babel", [mkdir("cli", [mkfile("LICENSE")])])], { owner: "root", hidden: false }),
-    ],
-    { hidden: true }
-  );
+const compressImages = (directoty) => {
+  const children = getChildren(directoty);
+  const newChildren = children.map((child) => {
+    const newMeta = _.cloneDeep(getMeta(child));
+    if (isDirectory(child)) {
+      return mkdir(getName(child), newMeta);
+    }
+    if (isFile(child) && _.endsWith(getName(child), ".jpg")) {
+      newMeta.size = getMeta(child).size / 2;
+    }
+    return mkfile(getName(child), newMeta);
+  });
 
-  return tree;
+  const newMeta = _.cloneDeep(getMeta(directoty));
+  const newDirectory = mkdir(getName(directoty), newChildren, newMeta);
+
+  return newDirectory;
 };
 
-// console.log(JSON.stringify(generate(), null, "  "));
-console.log(generate());
+const tree = mkdir("my documents", [
+  mkdir("documents.jpg"),
+  mkfile("avatar.jpg", { size: 100 }),
+  mkfile("passport.jpg", { size: 200 }),
+  mkfile("family.jpg", { size: 150 }),
+  mkfile("addresses", { size: 125 }),
+  mkdir("presentations"),
+]);
+
+const newTree = compressImages(tree);
+
+console.log(newTree);
+// console.log(JSON.stringify(newTree, null, " "));
